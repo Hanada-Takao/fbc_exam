266dc782568d67e301e535a41913e124f9839023
Hanada-Takao <taaa0823@gmail.com>
2021-12-09 19:16:17 +0900

    index_add_email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_090612) do
+ActiveRecord::Schema.define(version: 2021_12_09_101027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_090612) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "blogs", "users"
